1f51c5f906624a831211200c276bb36859795755
Elena Ru <elenaru@hotmail.de>
2021-02-02 14:55:00 +0100

    full solutions 1-7

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b) { return a-b;})
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.findIndex(item => item === pos) !== -1) {
+      const index = this.items.findIndex(item => item === pos)
+      return index;
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+      return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+      return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } 
+    const sumAr = this.items.reduce(function (acc, val) {
+      return acc + val;
+    })
+    return sumAr;
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+    const sumAr = this.items.reduce(function (acc, val) {
+      return acc + val;
+    })
+    return sumAr/this.items.length;
+  }
 }
+
+const listSorted = new SortedList;
 
 module.exports = SortedList;
